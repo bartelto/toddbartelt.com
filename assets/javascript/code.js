ea1cfb8906d58548a785b4bf6516876133ce2bcf
Miles Standoffish <toddnan@gmail.com>
2019-08-04 00:18:10 -0500

    single page site, plus updated portfolio styling

---
+++ assets/javascript/code.js
@@ -0,0 +1,40 @@
+// Add scrollspy to <body>
+$('body').scrollspy({target: ".navbar", offset: 50});
+
+// Add smooth scrolling on all links inside the navbar
+$("#my-navbar a").on('click', function(event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+
+  } // End if
+
+});
+
+$(".portfolio-item p").hide();
+
+$(".portfolio-item").mouseenter(function() {
+    console.log("portfolio hover");
+    $(this).children("a").children(".caption").children("p").show(500);
+});
+
+$(".portfolio-item").mouseleave(function() {
+    console.log("portfolio hover");
+    $(this).children("a").children(".caption").children("p").hide(500);
+});
